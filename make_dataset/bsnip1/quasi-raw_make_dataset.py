f0ff00d215f0885cbe142c59a7d389079394a9ea
PierreAuriau <p.auriau@hotmail.fr>
2022-05-14 11:41:54 +0200

    modify cat12 version

--- make_dataset/bsnip1/quasi-raw_make_dataset.py
+++ make_dataset/bsnip1/quasi-raw_make_dataset.py
@@ -17,7 +17,8 @@ import sys
 import numpy as np
 import pandas as pd
 
-sys.path.append(os.path.abspath('..'))
+path_to_file = os.path.abspath(os.path.dirname(sys.argv[0]))
+sys.path.append(os.path.dirname(path_to_file))
 from make_dataset_utils import quasi_raw_nii2npy
 
 # path to neurospin
@@ -31,7 +32,7 @@ else:
 study = "bsnip1"
 
 regex = "derivatives/quasi-raw/sub-*/ses*/anat/*preproc-linear*.nii.gz"
-qc_file = "derivatives/cat12-12.7_vbm_qc/qc.tsv"
+qc_file = "derivatives/cat12-12.6_vbm_qc_2018/qc.tsv"
 
 output_path = op.join(prefixe, 'neurospin/dico/pauriau/data/cohorts', study)
 
@@ -46,5 +47,5 @@ qc = op.join(study_dir, qc_file)
 
 # Array creation
 quasi_raw_nii2npy(nii_path, phenotype, dataset_name, output_path, qc=qc, sep='\t', id_type=str,
-                 check = dict(shape=(182, 218, 182), zooms=(1, 1, 1)), resampling=1.5)
+                check = dict(shape=(182, 218, 182), zooms=(1, 1, 1)), resampling=1.5)
 
