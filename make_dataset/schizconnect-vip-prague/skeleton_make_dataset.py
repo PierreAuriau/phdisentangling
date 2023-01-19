1461dc659cf5600cac2da6287cc27a737fd9bb62
PierreAuriau <p.auriau@hotmail.fr>
2023-01-19 11:55:48 +0100

    update with new skeleton function

--- make_dataset/schizconnect-vip-prague/skeleton_make_dataset.py
+++ make_dataset/schizconnect-vip-prague/skeleton_make_dataset.py
@@ -3,7 +3,7 @@
 """
 Created on Tue Aug  9 16:41:10 2022
 
-@author: pa267054
+@author: Pierre Auriau
 """
 import os, sys
 # Make dataset
@@ -11,40 +11,49 @@ sys.path.append(os.path.abspath('..'))
 from make_dataset_utils import skeleton_nii2npy
 import pandas as pd
 
-######## TO DELETE ########
-if os.path.exists('/home/pa267054/neurospin'):
-    prefixe = '/home/pa267054'
-else:
-    prefixe = '/'
-###########################
 
 study = 'schizconnect-vip-prague'
 
 # Directories
-study_dir = os.path.join(prefixe, 'neurospin', 'psy_sbox', study)
+neurospin = "/neurospin"
 
-output_dir = os.path.join(prefixe, "neurospin", "psy_sbox", "analyses", "202205_predict_neurodev", "data")
+study_dir = os.path.join(neurospin, 'psy_sbox', study)
 
-# Directory wher resample skeleton files are
+output_dir = os.path.join(neurospin, "psy_sbox", "analyses", "202205_predict_neurodev", "data")
+
+# Directory where resample skeleton files are
 voxel_size = 1.5
-resampled_skeleton_dir = os.path.join(output_dir, study, "skeleton", str(voxel_size)+"mm")
+#resampled_skeleton_dir = os.path.join(output_dir, "skeletons", study, str(voxel_size)+"mm")
+resampled_skeleton_dir = os.path.join(output_dir, "skeletons", study, str(voxel_size)+"mm", "wo_ventricles")
 
 
 
 ### Creation of skeleton array ###
 
 # Parameters
-regex = "?/?resampled_skeleton_sub-*_ses-*.nii.gz"
+side = "F"
+
+#regex = "?/?resampled_full_skeleton_sub-*_ses-*.nii.gz"
+regex = f"{side}/{side}resampled_skeleton_sub-*_ses-*.nii.gz"
 nii_path = os.path.join(resampled_skeleton_dir, regex)
-qc = {"vbm": os.path.join(study_dir, "derivatives", "cat12-12.7_vbm_qc", "qc.tsv")}
-output_path = os.path.join(output_dir, study, "skeleton")
-side = "both"
+
+#output_path = os.path.join(output_dir, "skeletons", study)
+output_path = os.path.join(output_dir, "skeletons", study, "wo_ventricles")
+
+qc_filename = os.path.join(study_dir, "derivatives", "cat12-12.6_vbm_qc", "qc.tsv")
+qc_file = pd.read_csv(qc_filename, sep='\t')
+qc_file["session"] = qc_file["session"].replace(1, "v1")
+
+qc = {"vbm": qc_file}
 check = {"shape": (128, 152, 128), 
          "zooms": (1.5, 1.5, 1.5)}
- 
+
 phenotype_filename = os.path.join(study_dir, 'participants.tsv')
 phenotype = pd.read_csv(phenotype_filename, sep='\t')
 phenotype["session"] = phenotype["session"].fillna("v1")
+
+assert phenotype["study"].isnull().values.any()
+assert phenotype["site"].isnull().values.any()
 
 # Array creation
 skeleton_nii2npy(nii_path=nii_path, 
