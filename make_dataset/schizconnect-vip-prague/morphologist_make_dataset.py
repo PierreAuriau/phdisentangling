04149006246d921356febe1350490ee8ca44aed2
PierreAuriau <p.auriau@hotmail.fr>
2023-01-19 11:59:50 +0100

    add bids parameter

--- make_dataset/schizconnect-vip-prague/morphologist_make_dataset.py
+++ make_dataset/schizconnect-vip-prague/morphologist_make_dataset.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-@author: Pierre Auriau
-
 
 Pipeline to create skeleton dataset for the SCHIZCONNECT-VIP-PRAGUE study
 To launch the script, you need to be in the brainvisa container and to install deep_folding
@@ -17,42 +15,35 @@ from deep_folding.brainvisa.generate_skeletons import generate_skeletons
 from deep_folding.brainvisa.generate_ICBM2009c_transforms import generate_ICBM2009c_transforms
 from deep_folding.brainvisa.resample_files import resample_files
 
-######## TO DELETE ########
-if os.path.exists('/home/pa267054/neurospin'):
-    prefixe = '/home/pa267054'
-else:
-    prefixe = '/'
-###########################
-
 study = 'schizconnect-vip-prague'
 
 # Directories
-study_dir = os.path.join(prefixe, 'neurospin', 'psy_sbox', study)
+neurospin = "/neurospin"
+study_dir = os.path.join(neurospin, 'psy_sbox', study)
 morpho_dir = os.path.join(study_dir, 'derivatives', 'morphologist-2021')
 
-output_dir = os.path.join(prefixe, "neurospin", "psy_sbox", "analyses", "202205_predict_neurodev", "data")
+#output_dir = os.path.join(neurospin, "psy_sbox", "analyses", "202205_predict_neurodev", "data")
+output_dir = os.path.join(neurospin, "dico", "data", "deep_folding", "current", "datasets")
 
 
 ### Preprocessing with deep_folding ###
 
 ## Parameters
 
-voxel_size = 1.5 #resampled voxel size
+voxel_size = 1.5  # resampled voxel size
 
-junction = "thin" # "wide" or "thin"
-
-session = True
-run = False
+junction = "thin"  # "wide" or "thin"
 
 # For debugging, put parallel=False, number_subjects=1
 parallel = True
-number_subjects = -1 # all subjects
+number_subjects = -1  # all subjects : -1
+bids = True
 
 # Morphologist directory containing the subjects as subdirectories
 src_dir = os.path.join(morpho_dir, 'subjects')
 
 # Output directory where to put raw skeleton
-skeleton_dir = os.path.join(output_dir, study, "skeleton", "raw")
+skeleton_dir = os.path.join(output_dir, study, "skeletons", "raw")
 
 # Relative path to graph for biobd dataset
 path_to_graph = "t1mri/ses-*/default_analysis/folds/3.1"
@@ -74,9 +65,8 @@ for side in ["L", "R"]:
                        junction=junction,
                        parallel=parallel,
                        number_subjects=number_subjects,
-                       session=session,
-                       run=run)
-    
+                       bids=bids)
+    """
     #Generate transform files
     generate_ICBM2009c_transforms(src_dir=src_dir,
                                   transform_dir=transform_dir,
@@ -84,8 +74,7 @@ for side in ["L", "R"]:
                                   side=side,
                                   parallel=parallel,
                                   number_subjects=number_subjects,
-                                  session=session,
-                                  run=run)
+                                  bids=bids)
     
     #Resample skeletons in the ICBM2099c template
     resample_files(src_dir=skeleton_dir,
@@ -95,4 +84,6 @@ for side in ["L", "R"]:
                    side=side,
                    number_subjects=number_subjects,
                    out_voxel_size=voxel_size,
-                   parallel=parallel)
+                   parallel=parallel,
+                   bids=bids)
+    """
