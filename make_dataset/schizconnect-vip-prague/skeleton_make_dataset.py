9793f5ee3e0555173617774111e3ef1219f319b4
PierreAuriau <p.auriau@hotmail.fr>
2023-05-31 17:21:20 +0200

    update for the new version of deep_folding

--- make_dataset/schizconnect-vip-prague/skeleton_make_dataset.py
+++ make_dataset/schizconnect-vip-prague/skeleton_make_dataset.py
@@ -10,7 +10,7 @@ import os, sys
 sys.path.append(os.path.abspath('..'))
 from make_dataset_utils import skeleton_nii2npy
 import pandas as pd
-
+import numpy as np
 
 study = 'schizconnect-vip-prague'
 
@@ -19,41 +19,53 @@ neurospin = "/neurospin"
 
 study_dir = os.path.join(neurospin, 'psy_sbox', study)
 
-output_dir = os.path.join(neurospin, "psy_sbox", "analyses", "202205_predict_neurodev", "data")
+output_dir = os.path.join(neurospin, "psy_sbox", "analyses", "202205_predict_neurodev", 
+                          "data", "skeletons", study)
 
 # Directory where resample skeleton files are
 voxel_size = 1.5
-#resampled_skeleton_dir = os.path.join(output_dir, "skeletons", study, str(voxel_size)+"mm")
-resampled_skeleton_dir = os.path.join(output_dir, "skeletons", study, str(voxel_size)+"mm", "wo_ventricles")
-
-
+resampled_skeleton_dir = os.path.join(output_dir, str(voxel_size)+"mm", "wo_ventricles")
 
 ### Creation of skeleton array ###
 
 # Parameters
 side = "F"
+skeleton_size = True
+stored_data = False
 
 #regex = "?/?resampled_full_skeleton_sub-*_ses-*.nii.gz"
-regex = f"{side}/{side}resampled_skeleton_sub-*_ses-*.nii.gz"
-nii_path = os.path.join(resampled_skeleton_dir, regex)
+regex = f"{side}resampled_skeleton_sub-*_ses-*.nii.gz"
+nii_path = os.path.join(resampled_skeleton_dir, side, regex)
 
-#output_path = os.path.join(output_dir, "skeletons", study)
-output_path = os.path.join(output_dir, "skeletons", study, "wo_ventricles")
+output_path = os.path.join(output_dir, "arrays", "with_skeleton_size")
 
-qc_filename = os.path.join(study_dir, "derivatives", "cat12-12.6_vbm_qc", "qc.tsv")
-qc_file = pd.read_csv(qc_filename, sep='\t')
-qc_file["session"] = qc_file["session"].replace(1, "v1")
+qc_vbm_filename = os.path.join(study_dir, "derivatives", "cat12-12.6_vbm_qc", "qc.tsv")
+qc_vbm = pd.read_csv(qc_vbm_filename, sep='\t')
+qc_vbm["session"] = qc_vbm["session"].replace(1, "v1")
 
-qc = {"vbm": qc_file}
+qc_skel_filename = os.path.join(output_dir, "metadata", f"{study}_morphologist_qc.tsv")
+qc_skel = pd.read_csv(qc_skel_filename, sep='\t')
+qc_skel["session"] = qc_skel["session"].replace(1, "v1")
+
+qc = pd.merge(qc_vbm, qc_skel, how="outer", on=["participant_id", "session", "run"],
+              validate="1:1", suffixes=("_vbm", "_skeleton"))
+qc["qc_vbm"] = qc["qc_vbm"].fillna(0)
+qc["qc_skeleton"] = qc["qc_skeleton"].fillna(0)
+qc["qc"] = qc[["qc_vbm", "qc_skeleton"]].prod(axis=1).astype(int)
+
 check = {"shape": (128, 152, 128), 
-         "zooms": (1.5, 1.5, 1.5)}
+        "voxel_size": (1.5, 1.5, 1.5),
+        "transformation": np.array([-1, 0, 0, 96, 0, -1, 0, 96, 0, 0, -1, 114, 0, 0, 0, 1]),
+        "storage": np.array([-1, 0, 0, 127, 0, -1, 0, 151, 0, 0, -1, 127, 0, 0, 0, 1])}
 
 phenotype_filename = os.path.join(study_dir, 'participants.tsv')
 phenotype = pd.read_csv(phenotype_filename, sep='\t')
 phenotype["session"] = phenotype["session"].fillna("v1")
+phenotype = phenotype.rename(columns={"TIV": "tiv"})
 
-assert phenotype["study"].isnull().values.any()
-assert phenotype["site"].isnull().values.any()
+assert phenotype["study"].notnull().values.all(), "study column in phenotype has nan values"
+assert phenotype["site"].notnull().values.all(), "site column in phenotype has nan values"
+assert phenotype["tiv"].notnull().values.all(), "tiv column in phenotype has nan values"
 
 # Array creation
 skeleton_nii2npy(nii_path=nii_path, 
@@ -64,4 +76,6 @@ skeleton_nii2npy(nii_path=nii_path,
                   sep='\t', 
                   id_type=str,
                   check=check, 
-                  side=side)
+                  side=side,
+                  skeleton_size=skeleton_size,
+                  stored_data=stored_data)
